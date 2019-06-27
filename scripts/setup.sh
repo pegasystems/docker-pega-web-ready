e3ddf729cf5f2fc4b9f772e0425c82dbf1af3718
kants3 <shashi.kant@in.pega.com>
2019-06-27 16:32:44 +0500

    test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,4 +11,4 @@ git remote show origin
 
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
 
-container-structure-test test --image pegasystems/pega-ready --config scripts/testcases.yaml
+container-structure-test test --image $1/$2 --config scripts/testcases.yaml
