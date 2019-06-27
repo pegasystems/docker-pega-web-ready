e6da4c8f8da5ef1931c23e971dd65c2e4de42d8c
kants3 <shashi.kant@in.pega.com>
2019-06-27 16:59:25 +0500

    tst'

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,6 +9,9 @@ git config --get remote.origin.url
 
 git remote show origin
 
+echo 'First arg $1'
+echo 'Second arg $2'
+
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
 
 container-structure-test test --image $1/$2 --config scripts/testcases.yaml
