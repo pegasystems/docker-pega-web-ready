02d99816b0ca0d0742441c3b9212303a2c3bdc35
arugm <Madhuri.Arugula@in.pega.com>
2019-07-02 16:17:01 +0530

    moved to test directory

---
+++ tests/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+
+
+echo $1
+echo $2
+
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
+
+container-structure-test test --image $1/$2 --config /tests/pega-web-ready-testcases.yaml
