003b78869159ac8d712a26b14b92654bd8ee5a0d
hickk <Kevin.Hickey@pega.com>
2021-03-16 14:36:28 -0400

    US-401331
    Adding check to see ownership of gosu
    Changed gosu call back to tomcat user

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -256,11 +256,12 @@ unset DB_USERNAME DB_PASSWORD SECRET_DB_USERNAME SECRET_DB_PASSWORD CASSANDRA_US
 
 unset pega_root lib_root config_root
 
-# Run tomcat (as tomcat, 9002) if the first argument is run otherwise try to run whatever the argument is a command
-set -- gosu tomcat "$@"
-if [ "$1" = 'run' ]; then
-  exec "${CATALINA_HOME}/bin/catalina.sh" "$@"
-else
-  exec "$@"
-fi
+echo "Checking gosu ownership"
+ls -la | grep gosu
 
+# Run tomcat (as tomcat, 9002) if the first argument is run otherwise try to run whatever the argument is a command
+if [ "$1" = 'run' ]; then
+  exec gosu tomcat "${CATALINA_HOME}/bin/catalina.sh" "$@"
+else
+  exec gosu tomcat "$@"
+fi
