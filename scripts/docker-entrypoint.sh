864063b7643c695660c5a01a18ee839ec3291f72
hickk <Kevin.Hickey@pega.com>
2021-03-16 14:51:50 -0400

    US-401331
    Fixed gosu path

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -257,7 +257,7 @@ unset DB_USERNAME DB_PASSWORD SECRET_DB_USERNAME SECRET_DB_PASSWORD CASSANDRA_US
 unset pega_root lib_root config_root
 
 echo "Checking gosu ownership"
-ls -la | grep gosu
+ls -la /usr/sbin/gosu
 
 # Run tomcat (as tomcat, 9002) if the first argument is run otherwise try to run whatever the argument is a command
 if [ "$1" = 'run' ]; then
