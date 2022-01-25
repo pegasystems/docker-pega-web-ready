76b3f3ede8c724fefd3b750ff4136db6290125c0
somuh <HarshaVardhan.Somu@in.pega.com>
2022-01-25 12:48:00 +0530

    US-457459 Restrict deployment options to c/s for k8s enviorments

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -172,11 +172,9 @@ else
    export SECRET_HZ_CS_AUTH_PASSWORD=${HZ_CS_AUTH_PASSWORD}
 fi
 
-if [ "HZ_CLIENT_MODE" == true ]; then
-    if [ "$SECRET_HZ_CS_AUTH_USERNAME" == "" ] || [ "$SECRET_HZ_CS_AUTH_PASSWORD" == "" ]; then
-        echo "HZ_CS_AUTH_USERNAME & HZ_CS_AUTH_PASSWORD must be specified in hazelcast client server mode deployments.";
-        exit 1
-    fi
+if [ "$SECRET_HZ_CS_AUTH_USERNAME" == "" ] || [ "$SECRET_HZ_CS_AUTH_PASSWORD" == "" ]; then
+    echo "HZ_CS_AUTH_USERNAME & HZ_CS_AUTH_PASSWORD must be specified in hazelcast client server mode deployments.";
+    exit 1
 fi
 
 /bin/dockerize -template ${CATALINA_HOME}/webapps/ROOT/index.html:${CATALINA_HOME}/webapps/ROOT/index.html
