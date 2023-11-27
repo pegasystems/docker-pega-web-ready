a7846377ad18abbf62ab6885fbfd641f3cec0d2e
kumaa43 <athesh.kumar@in.pega.com>
2023-11-27 12:33:50 +0000

    US-572740 - code changes

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -45,6 +45,7 @@ catalina_properties="${config_root}/catalina.properties"
 prbootstrap_properties="${config_root}/prbootstrap.properties"
 java_security_overwrite="${config_root}/java.security.overwrite"
 tomcat_web_xml="${config_root}/tomcat-web.xml"
+file_setting_download_script="${lib_root}/filesettingdownload.sh"
 
 declare -a secrets_list=("DB_USERNAME" "DB_PASSWORD" "CUSTOM_ARTIFACTORY_USERNAME" "CUSTOM_ARTIFACTORY_PASSWORD" "CUSTOM_ARTIFACTORY_APIKEY_HEADER" "CUSTOM_ARTIFACTORY_APIKEY" "CASSANDRA_USERNAME" "CASSANDRA_PASSWORD" "CASSANDRA_TRUSTSTORE_PASSWORD" "CASSANDRA_KEYSTORE_PASSWORD"  "HZ_CS_AUTH_USERNAME" "HZ_CS_AUTH_PASSWORD" "PEGA_DIAGNOSTIC_USER" "PEGA_DIAGNOSTIC_PASSWORD" "STREAM_TRUSTSTORE_PASSWORD" "STREAM_KEYSTORE_PASSWORD" "STREAM_JAAS_CONFIG")
 for secret in "${secret_root}"/*
@@ -114,6 +115,14 @@ if [[ "$custom_artifactory_auth" == "" && ( "$SECRET_CUSTOM_ARTIFACTORY_APIKEY_H
     fi
 fi
 
+#for file settings in pega cloudk
+if [ -e "$file_setting_download_script" ]; then
+  echo "file setting script does exists"
+  (cd $pega_root ; sh "$file_setting_download_script")
+else
+   echo "file setting script does not exist"
+fi
+
 custom_artifactory_certificate=''
 if [ "$(ls -A "${pega_root}/artifactory/cert"/*)" ]; then
      if [ "$(ls "${pega_root}/artifactory/cert"/* | wc -l)" == 1 ]; then
@@ -176,7 +185,7 @@ do
     fi
 done
 
-echo "Using JDBC_URL: ${JDBC_URL}"
+echo "Using JDBC_URL: ${c}"
 
 # import certificates to jvm keystore
 for certfile in "${pega_root}/certs"/*
