1e1ac79da141cb4636dd85250f46158aa1a3226b
chita1 <akshitha.chittanoori@in.pega.com>
2025-05-13 00:37:48 +0530

    Added tests

--- tomcat-bin/setenv.sh
+++ tomcat-bin/setenv.sh
@@ -41,11 +41,15 @@ else
   echo "No krb5.conf was specified in ${krb5_conf}."
 fi
 
+
 # Adding Pega RASP agent jar to JAVA_OPTS
 pega_rasp_agent_root="/opt/pega/rasp"
 if [ "${IS_PEGA_25_OR_LATER}" == "true" ] && [ "${IS_RASP_ENABLED}" == "true" ]; then
   echo "Adding ${pega_rasp_agent_root} agent jar to JAVA_OPTS";
-  JAVA_OPTS="${JAVA_OPTS} -javaagent:${pega_rasp_agent_root}/pegarasp-agent.jar=action=${RASP_ACTION} -Xbootclasspath/a:${pega_rasp_agent_root}/pegarasp-core.jar"
+  if [ -n "${RASP_ACTION}" ]; then
+    pega_rasp_action="=action=${RASP_ACTION}"
+  fi
+  JAVA_OPTS="${JAVA_OPTS} -javaagent:${pega_rasp_agent_root}/pegarasp-agent.jar${pega_rasp_action} -Xbootclasspath/a:${pega_rasp_agent_root}/pegarasp-core.jar"
 else
   echo "RASP is disabled, not adding RASP agent jar to JAVA_OPTS"
 fi
