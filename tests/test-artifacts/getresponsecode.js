173b726247f155d8b3377c29f4f6b9d3328ee168
misterdorito <mrdorito@gmail.com>
2026-06-03 12:01:39 -0400

    Replace use of jrunscript with java SourceFile.java style invocations.

--- tests/test-artifacts/getresponsecode.js
+++
@@ -1,6 +0,0 @@
-var url = arguments[0];
-var client = java.net.http.HttpClient.newHttpClient();
-var request = java.net.http.HttpRequest.newBuilder().uri(java.net.URI.create(url)).GET().build();
-var response = client.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
-var statusCode = response.statusCode();
-println(statusCode);
